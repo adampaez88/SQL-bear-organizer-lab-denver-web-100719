1b55051aceadeeaece732f3534ee3c8296403be2
Adam chernitsky <adamlee2388@gmail.com>
2020-03-24 07:57:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('griz', 7, 'male', 'black and white', 'rowdy and loving', true);
