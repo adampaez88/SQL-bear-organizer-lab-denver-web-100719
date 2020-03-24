78bde7b559bf602672ef54654e9c0273344731ef
Adam chernitsky <adamlee2388@gmail.com>
2020-03-24 08:15:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'male', 'black and white', 'rowdy and loving', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 6, 'male', 'brown', 'hyper', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'female', 'black', 'shy', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'male', 'white', 'suspicious', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'brown', 'curious', true);
+
 INSERT INTO bears (name, age ,gender, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'green', 'mean', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 1, 'female', 'black', 'rude', true);
+
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, 'M', 'black', 'agressive', false);
