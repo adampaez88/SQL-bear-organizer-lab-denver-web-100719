d8b1e0938fa86dce9507e902a2a75c697b3300a8
Adam chernitsky <adamlee2388@gmail.com>
2020-03-24 08:47:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,14 +2,14 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Ch
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'brown', 'hyper', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'black', 'shy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'black', 'shy', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'white', 'suspicious', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'brown', 'curious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'brown', 'curious', 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'green', 'mean', 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'black', 'rude', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'black', 'rude', 1);
 
 INSERT INTO bears (age, gender, color, temperament, alive) VALUES (20, 'M', 'black', 'agressive', 0);
